538f3411a0e1c4fd75dbfd72924cc64e63ed0c12
NeaBr <122275401+NeaBr@users.noreply.github.com>
2024-01-29 10:17:11 +0200

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,38 @@
+
+export const DATA = [
+    {
+        id: "1",
+        lastname: 'Juntunen',
+        firstname: 'Jaakko'
+    },
+    {
+        id: '2',
+        lastname: 'Ojala',
+        firstname: 'Liisa'
+    },
+    {
+        id: '3',
+        lastname: 'Koilinen',
+        firstname: 'Jouni'
+    },
+    {
+        id: '4',
+        lastname: 'Kaakko',
+        firstname: 'Aliisa'
+    },
+    {
+        id: '5',
+        lastname: 'Lounas',
+        firstname: 'Verneri'
+    },
+    {
+        id: '6',
+        lastname: 'Luode',
+        firstname: 'Jussi'
+    },
+    {
+        id: '7',
+        lastname: 'Kuolema',
+        firstname: 'Sara'
+    },
+]
